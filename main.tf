49f7bcde75316deb816d3ea172b49be5be2e7876
Omar <omar.din@youlend.com>
2022-06-13 14:38:40 +0100

    fix: rds-monitoring: rds iam policy fix

--- main.tf
+++ main.tf
@@ -150,7 +150,7 @@ resource "aws_iam_policy" "datadog-rds-monitoring" {
         "rds:DescribeEvents",
       ],
       "Effect": "Allow",
-      "NotResource": ${jsonencode(var.excluded_rds_arn)}
+      "NotResource": [ ${join(",", [for s in var.excluded_rds_arn : format("%q", s)])} ]
     }
   ]
 }
